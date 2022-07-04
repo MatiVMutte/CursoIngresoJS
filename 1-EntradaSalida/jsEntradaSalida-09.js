2743063d1104e5a6f720806fe57b31f063a50747
MatiVMutte <mativmutte@gmail.com>
2022-07-04 12:44:54 -0300

    REALIZADO - FIRMADO

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,17 @@
 /*
+Reta Mutte Matias | DIV D
+E/S Ejer 9
+
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let importe = parseInt(document.getElementById("txtIdSueldo").value);
 
-	alert("ok");
+	let aumento = (importe * 10) / 100 ;
+	let total = importe + aumento;
+
+	txtIdResultado.value = total;
 }
