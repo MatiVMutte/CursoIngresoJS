a448b1a40a45c4427a9ab3c6f3d67db819a1f572
MatiVMutte <mativmutte@gmail.com>
2022-07-04 11:44:18 -0300

    REALIZADO - FIRMADO

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,17 @@
 /*
+Reta Mutte Matias | DIV D
+E/S Ejer 6
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	let num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	let suma = num1 + num2;
+
+	alert(num1 + " + " + num2 + " = " + suma);
 }
 
