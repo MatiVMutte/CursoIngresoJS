a98d4f14166bb2c70f05f9b64714695eae178b1b
MatiVMutte <mativmutte@gmail.com>
2022-07-04 12:39:39 -0300

    REALIZADO - FIRMADO

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,17 @@
 /*
+Reta Mutte Matias | DIV D
+E/S Ejer 8
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let n1 = parseInt(document.getElementById("txtIdNumeroDividendo").value);
+	let n2 = parseInt(document.getElementById("txtIdNumeroDivisor").value);
+
+	let resto = n1 % n2;
+
+	alert("El resto es: " + resto);
 }
