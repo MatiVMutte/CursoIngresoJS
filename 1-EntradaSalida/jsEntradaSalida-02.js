3cde71bf8c2df59fe138149128421ea3e414b9af
MatiVMutte <mativmutte@gmail.com>
2022-07-04 10:30:12 -0300

    REALIZADO

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,4 +1,11 @@
 /*
+Reta Mutte Matias
+E/S Ejercicio 2
+
+Al presionar el botón 'Mostrar'
+debemos lograr tomar un dato por '=prompt()'
+y luego mostrarlo por 'alert()'.
+
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
