f95bc16909dda36f9af6a998081866b797eca6be
MatiVMutte <mativmutte@gmail.com>
2022-07-05 14:21:37 -0300

    REALIZADO - FIRMADO

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,4 +1,8 @@
-/*3.	Para el departamento de Pinturas:
+/*
+    Reta Mutte Matias | DIV D
+    TP N°1 Ejer 3
+
+3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
