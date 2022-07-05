5e9060f4f63af462388e498ca5ed35d75f5d73d8
MatiVMutte <mativmutte@gmail.com>
2022-07-05 10:06:17 -0300

    REALIZADO - FIRMADO

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,22 @@
+/*
+	Reta Mutte Matias | DIV D
+	IF Ejer 6
+
+	Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) 
+	o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-
-}//FIN DE LA FUNCIÓN
+	if(edad >= 18) {
+		alert("Es Mayor de edad.");
+	} else if(edad >= 13 && edad <=17) {
+		alert("Es Adolescente.");
+	} else {
+		alert("Es un ninio.");
+	}
+}
