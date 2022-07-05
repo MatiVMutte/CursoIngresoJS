ac1d89b400f286823d12a34d7bd42a8cf1b66309
MatiVMutte <mativmutte@gmail.com>
2022-07-05 13:09:15 -0300

    REALIZADO - FIRMADO

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -10,27 +10,36 @@ function mostrar()
 {
 	let edad;
 	let select;
-	let textSelect;
+	let estadoCivil;
 
-	edad = document.getElementById("txtIdEdad").value;
-	select = document.getElementById("estadoCivil");
+	edad = document.getElementById("txtIdEdad").value; // Obtengo la edad del campo INPUT tipo Texto
+	select = document.getElementById("estadoCivil"); // Obtengo el elemento del campo SELECT para posteriormente acceder a el elemento seleccionado
 
-	if(edad != isNaN(edad)) {
-		edad = parseInt(edad);
-		textSelect = select.options[select.selectedIndex].text.toLowerCase();
+	if(edad != ""){ // Verifico Si El Campo Esta Vacio
+		if(!isNaN(edad)) { // Si el campo NO esta vacio. Verifico si (edad) sea un numero. 
+			edad = parseInt(edad); // Entonces convierto el "String" edad a un "Int"
+			estadoCivil = select.options[select.selectedIndex].text; // Obtengo en "String" el texto que esta en option seleccionado
+			estadoCivil = estadoCivil.toLowerCase(); // Convierto todo ese texto obtenido en MINUSCULAS(lowerCase) para evitar problemas en el IF
 
-		if(edad < 18 && textSelect != "soltero") {
-			alert("Es muy pequenio para NO ser soltero.");
+			if(edad < 18 && estadoCivil != "soltero") { // Si la persona es menor de edad AND/Y su estado civil es distinto a "Soltero"...
+				alert("Es muy pequenio para NO ser soltero.");
+
+			} else { // Si no...
+				alert("Muy bien!!");
+
+			}
+
 		} else {
-			alert("Muy bien!!");
+			alert("Debe poner su edad.");
+
 		}
 	} else {
-		alert("Debe poner su edad.");
-	}
+		alert("Campo EDAD vacio");
 
+	}
 	
 
     //alert(seleccion.selectedIndex); --> Me devuelve el indice del select seleccionado
-    //alert(seleccion.options[seleccion.selectedIndex].value); --> Para obtener el "value"
-	//alert(seleccion.options[seleccion.selectedIndex].text); --> Para obtener el "texto
+    //alert(seleccion.options[seleccion.selectedIndex].value); --> Para obtener el value=""
+	//alert(seleccion.options[seleccion.selectedIndex].text); --> Para obtener el "texto"
 }
