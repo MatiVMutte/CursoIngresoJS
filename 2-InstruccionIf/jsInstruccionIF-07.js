9db6499c2fa72ff7895ccb877a883f45d87bfa88
MatiVMutte <mativmutte@gmail.com>
2022-07-05 12:38:34 -0300

    REALIZADO - FIRMADO

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -8,11 +8,28 @@
 
 function mostrar()
 {
+	let edad;
+	let select;
+	let textSelect;
+
+	edad = document.getElementById("txtIdEdad").value;
+	select = document.getElementById("estadoCivil");
+
+	if(edad != isNaN(edad)) {
+		edad = parseInt(edad);
+		textSelect = select.options[select.selectedIndex].text.toLowerCase();
+
+		if(edad < 18 && textSelect != "soltero") {
+			alert("Es muy pequenio para NO ser soltero.");
+		} else {
+			alert("Muy bien!!");
+		}
+	} else {
+		alert("Debe poner su edad.");
+	}
+
 	
 
-	//alert(seleccion.selectedIndex.typeof); --> undefined
-
-	
     //alert(seleccion.selectedIndex); --> Me devuelve el indice del select seleccionado
     //alert(seleccion.options[seleccion.selectedIndex].value); --> Para obtener el "value"
 	//alert(seleccion.options[seleccion.selectedIndex].text); --> Para obtener el "texto
