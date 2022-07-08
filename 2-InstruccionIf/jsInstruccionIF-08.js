e7ba35b60570cdab7825f5413147c25478aee188
MatiVMutte <mativmutte@gmail.com>
2022-07-08 10:47:08 -0300

    REALIZADO - FIRMADO

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,35 @@
+/*
+	Reta Mutte Matias | DIV D
+	IF Ejer 8
+
+	Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad;
+	let select;
+	let estadoCivil;
 
+	edad = document.getElementById("txtIdEdad").value;
+	select = document.getElementById("estadoCivil");
 
-}//FIN DE LA FUNCIÓN
+	if(edad != "") {
+		if(!isNaN(edad)) {
+			edad = parseInt(edad);
+			estadoCivil = select.options[select.selectedIndex].text;
+			estadoCivil = estadoCivil.toLowerCase();
+
+			if(edad < 18 && estadoCivil != "soltero") {
+				
+			} else if(edad > 18 && estadoCivil == "soltero") {
+				alert("Es soltero y no es menor");
+			}
+		} else{
+			alert("Debe ingresar su edad en numeros");
+		}
+	} else {
+		alert("Campo esta vacio");
+	}
+}
