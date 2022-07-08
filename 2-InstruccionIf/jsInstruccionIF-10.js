ccb1e7d7fca161842ecaafdf2ad32c301916a96d
MatiVMutte <mativmutte@gmail.com>
2022-07-08 12:58:22 -0300

    REALIZADO - FIRMADO

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,22 @@
+/*
+	Reta Mutte Matias | DIV D
+	IF Ejer 10
+
+	Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+	"EXCELENTE" para notas igual a 9 o 10,
+	"APROBÓ" para notas mayores a 4,
+	"Vamos, la proxima se puede" para notas menores a 4
+*/
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota = Math.floor(Math.random()*10+1);
 
-}//FIN DE LA FUNCIÓN
+	if(nota == 9 || nota == 10) {
+		alert(nota + ": EXCELENTE");
+	} else if(nota >= 4 && nota <= 8) {
+		alert(nota + ": APROBÓ");
+	} else if(nota >= 0 && nota < 4) {
+		alert(nota + ": Vamos!!");
+	}
+}
