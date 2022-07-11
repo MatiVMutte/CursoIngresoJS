d72f4ece686ad10389112ad2b231215d56e44876
MatiVMutte <mativmutte@gmail.com>
2022-07-11 12:13:36 -0300

    REALIZADO - ENTREGADO

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -2,7 +2,7 @@
 	Reta Mutte Matias | DIV D
 	IF Ejer 6
 
-	ENTREGADO
+	ENTREGADO - Corregido -- edad > 18 --> edad >= 18
 
 	Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) 
 	o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
@@ -15,7 +15,7 @@ function mostrar()
 	edad = document.getElementById("txtIdEdad").value;
 	edad = parseInt(edad);
 
-	if(edad > 18) {
+	if(edad >= 18) {
 		alert("Es Mayor de edad.");
 	} else if(edad > 12 && edad < 18) {
 		alert("Es Adolescente.");
