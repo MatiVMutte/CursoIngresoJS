c84e16075a0a525e40cf7faafc23505123301836
MatiVMutte <mativmutte@gmail.com>
2022-07-15 13:27:15 -0300

    REALIZADO - FIRMADO

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -7,20 +7,48 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
+var contadorIntentos = 0;
+var numeroIngresado;
+var comenzo = false;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+  if(!comenzo) {
+    numeroSecreto = Math.floor(Math.random() * 100 + 1);
 
+    alert("EL JUEGO HA COMENZADO! Ingrese un numero en el campo de abajo.");
+    comenzo = true;
+    document.getElementById("txtIdIntentos").value = "Intentos: " + contadorIntentos;
+  } else {
+    alert("El juego ya ha comenzado. Debe ingresar un numero.");
+  }
 }
 
 function verificar()
 {
-	
-	
+  if(comenzo) {
+    numeroIngresado = document.getElementById("txtIdNumero").value;
+
+    if(numeroIngresado != "" && !isNaN(numeroIngresado) && (numeroIngresado > 0 && numeroIngresado < 101)) {
+      numeroIngresado = parseInt(numeroIngresado);
+
+      if(numeroIngresado == numeroSecreto) {
+        alert("Usted es un ganador!!!\nY en solo " + contadorIntentos + " intentos.");
+        comenzo = false;
+      } else {
+        contadorIntentos++;
+        document.getElementById("txtIdIntentos").value = "Intentos: " + contadorIntentos;
+        if(numeroIngresado > numeroSecreto) {
+          alert("Te pasaste!!");
+        } else {
+          alert("Falta para llegarrr!!");
+        }
+      }
+    } else {
+        alert("Debe ingresar un numero entre ( 1 - 100).");
+    }
+
+  } else {
+      alert("Dale al boton \"Comenzar\"");
+  }
 }
