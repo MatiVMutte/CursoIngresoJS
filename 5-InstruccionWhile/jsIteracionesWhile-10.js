2b047e549156327897c524420d7a0b1191c6c995
MatiVMutte <mativmutte@gmail.com>
2022-07-25 18:15:33 -0300

    REALIZADO

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -1,77 +1,138 @@
-/*
-	Reta Mutte Matias | DIV D
-	WHILE Ejer 10
-
-	ENTREGADO
-
-	Al presionar el botón pedir  números  
-	hasta que el usuario quiera, mostrar:
-	1-Suma de los negativos.
-	2-Suma de los positivos.
-	3-Cantidad de positivos.
-	4-Cantidad de negativos.
-	5-Cantidad de ceros.
-	6-Cantidad de números pares.
-	7-Promedio de positivos.
-	8-Promedios de negativos.
-	9-Diferencia entre positivos y negativos, (positivos-negativos). 
+/* 
+Al presionar el botón pedir  números  
+hasta que el usuario quiera, mostrar:
+1-Suma de los negativos.
+2-Suma de los positivos.
+3-Cantidad de positivos.
+4-Cantidad de negativos.
+5-Cantidad de ceros.
+6-Cantidad de números pares.
+7-Promedio de positivos.
+8-Promedios de negativos.
+9-Diferencia entre positivos y negativos, (positvos-negativos). 
+10-El numero mas grande.
+11-De los pares el mas chiquito.
+12-De los negativos el mas grande, indicando el orden de ingreso.
 */
 function mostrar()
 {
-	//declarar contadores y variables 
-	let respuesta;
 	let numeroIngresado;
-	let sumaNegativos=0; // ?Listo
-	let sumaPositivos=0; // ?Listo
-	let cantPositivos=0; // ?Listo
-	let cantNegativos=0; // ?Listo
-	let cantCeros=0; // ?Listo
-	let cantNumPares=0; // ?Listo
-	let promedioPositivos; //?Listo
-	let promedioNegativos; //?Listo
-	let difCantPositivoNegativo; //?Listo
+	let respuesta;
+	let sumaNegativos;
+	let sumaPositivo;
+	let cantidadCeros;
+	let cantidadPositivos;
+	let cantidadNegativos;
+	let cantidadDePares;
+	let promedioPositivos;
+	let promedioNegativos;
+	let diferenciaNumeros;
+	let banderaMaxima;
+	let numeroNegativoMaximo;
+	let numeroMaximo;
+	let numeroParMinimo;
+    let orden;
+    let contador;
 
-	respuesta="si";
+	respuesta = "si";
+	sumaNegativos = 0;
+	sumaPositivo = 0;
+	cantidadCeros = 0;
+	cantidadNegativos = 0;
+	cantidadPositivos = 0;
+	cantidadDePares = 0;
+	banderaMaxima = false;
+	contador = 0;
 
-	while(respuesta=="si")
+	while(respuesta == "si")
 	{
 		numeroIngresado = prompt("Ingrese un numero");
 		numeroIngresado = parseInt(numeroIngresado);
 
-		if(numeroIngresado > 0) {
-			cantPositivos++;
-			sumaPositivos = sumaPositivos + numeroIngresado;
-		} else if(numeroIngresado < 0) {
-			cantNegativos++;
+		contador++;
+
+		if(numeroIngresado < 0)
+		{
 			sumaNegativos = sumaNegativos + numeroIngresado;
-		} else {
-			cantCeros++;
+			cantidadNegativos++;
+
+			if(numeroIngresado > numeroNegativoMaximo || cantidadNegativos == 1)
+			{
+				numeroNegativoMaximo = numeroIngresado;
+				orden = contador;
+			}
+		}
+		else
+		{
+			if(numeroIngresado > 0)
+			{
+				sumaPositivo = sumaPositivo + numeroIngresado;
+				cantidadPositivos++;
+			}
+			else
+			{
+				cantidadCeros++;
+			}
+		}
+		
+		if(numeroIngresado % 2 == 0)
+		{
+			cantidadDePares++;
+
+			if(numeroIngresado < numeroParMinimo || cantidadDePares == 1)
+			{
+				numeroParMinimo = numeroIngresado;
+			}	
+		}	
+		
+		if(numeroIngresado > numeroMaximo || banderaMaxima == false)
+		{
+			numeroMaximo = numeroIngresado;
+			banderaMaxima =  true;
 		}
 
-		if(numeroIngresado % 2 == 0) {
-			cantNumPares++;
-		}
+		respuesta = prompt("Desea seguir ingresando numeros?");
 
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+	}
 
-	promedioPositivos = sumaPositivos / cantPositivos;
-	promedioNegativos = sumaNegativos / cantNegativos;
+	diferenciaNumeros = sumaPositivo - sumaNegativos;
 
-	if(cantPositivos > cantNegativos) {
-		difCantPositivoNegativo = cantPositivos - cantNegativos;
-	} else {
-		difCantPositivoNegativo = cantNegativos -  cantPositivos;
+	document.write("La suma de los numeros positivos es: " + sumaPositivo + "<br>");
+	document.write("La suma de los numeros negativos es: " + sumaNegativos + "<br>");
+	document.write("La cantidad de positivos es: " + cantidadPositivos + "<br>");
+	document.write("La cantidad de negativos es: " + cantidadNegativos + "<br>");
+	document.write("La cantidad de ceros es de: " + cantidadCeros + "<br>");
+	document.write("La cantidad de numeros pares es: " + cantidadDePares + "<br>");
+	
+	if(cantidadNegativos == 0)
+	{
+		document.write("No se pudo calcular el promedio porque no se ingreso negativos" + "<br>");
+	}
+	else
+	{
+		promedioNegativos = sumaNegativos / cantidadNegativos;
+		document.write("El promedio de negativos es: " + promedioNegativos + "<br>");
+		document.write("El numero negativo maximo es: " + numeroNegativoMaximo + " y su orden es: " + orden + "<br>")
+	}
+	if(cantidadPositivos == 0)
+	{
+		document.write("No se pudo calcular el promedio porque no se ingreso positivos" + "<br>");
+	}
+	else
+	{
+		promedioPositivos = sumaPositivo / cantidadPositivos;
+		document.write("El promedio de positivos es: " + promedioPositivos + "<br>");
+	}
+	document.write("La diferencia entre los positivos y negativos es de: " + diferenciaNumeros + "<br>");
+	document.write("El numero maximo es: " + numeroMaximo + "<br>");
+	if(cantidadDePares == 0)
+	{
+		document.write("No se ingreso numeros pares");
+	}
+	else
+	{
+		document.write("El numero par mas chiquito es: " + numeroParMinimo + "<br>");
 	}
 	
-
-	document.write("La suma de positivos es :"+sumaPositivos+"<br>"
-					+"La suma de negativos es :"+sumaNegativos+"<br>"
-					+"La cantidad de positivos es: "+cantPositivos+"<br>"
-					+"La cantidad de negativos es: "+cantNegativos+"<br>"
-					+"La cantidad de ceros es: "+cantCeros+"<br>"
-					+"La cantidad de numeros pares es: "+cantNumPares+"<br><br>"
-					+"El promedio de los positivos es: "+promedioPositivos.toFixed(2)+"<br>"
-					+"EL promedio de los negativos es: "+promedioNegativos.toFixed(2)+"<br>"
-					+"La diferencia en cantidad de los positivos y los negativos es: "+difCantPositivoNegativo);
 }//FIN DE LA FUNCIÓN
+
