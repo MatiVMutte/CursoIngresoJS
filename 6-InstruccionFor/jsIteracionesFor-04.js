1e30ad91e49de0dd9a40c9eb4120004f0ee45249
MatiVMutte <mativmutte@gmail.com>
2022-07-25 18:41:10 -0300

    REALIZADO

--- 6-InstruccionFor/jsIteracionesFor-04.js
+++ 6-InstruccionFor/jsIteracionesFor-04.js
@@ -1,7 +1,15 @@
 function mostrar()
 {
-	alert("ok");
+	let repetir;
+	let respuesta;
+	repetir = true;
+	respuesta = true;
 
-
-
+	for(let i = 0; repetir; i++) {
+		if(respuesta == false) {
+			break; // Sale de la estructura
+		}
+		alert("Repeticion: " + (i+1));
+		respuesta = confirm("Desea seguir?");
+	}
 }//FIN DE LA FUNCIÓN
